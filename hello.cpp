2d63e214ca6581ae8d2eb0e444e7ff9d685c7ab6
juca004 <juca210784@gmail.com>
2017-03-28 16:08:56 -0600

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,22 @@
+#include <conio.h>
+#include <stdio.h>
+#include <iostream>
+
+main()
+
+{
+	int r,h,p;
+	float pre;
+	printf("T A B U L A D O R \n");
+	printf("Cuantos Recopiladores: \n");
+	scanf("%d",&r);
+
+	h = r*450;
+	p = h*2;
+	pre = p*.60;
+	
+	printf("El numero de hojas es: %d\n",h);	
+	printf("El numero de paginas es: %d\n",p);
+	printf("Usted tiene que pagar: %f\n",pre);
+system("pause");
+}
